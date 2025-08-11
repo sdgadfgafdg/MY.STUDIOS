4bca9730ddc26a5b43311a7c0c9ba78bfc69336c
Kostya <tempmailer@inbox.ru>
2025-08-11 19:59:26 +0300

    (((((((((((((((((((((((

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { config } from 'dotenv'
 
-dotenv
+config({})
 
 // https://vite.dev/config/
 export default defineConfig({
